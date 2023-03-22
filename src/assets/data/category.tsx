418c077e1e8d5a806ced019082c6f9a3e33817e7
geonheeMin <garnett0122@hansung.ac.kr>
2023-03-22 18:46:56 +0900

    0322

--- src/assets/data/category.tsx
+++ src/assets/data/category.tsx
@@ -1,9 +1,9 @@
 export const categories = [
-  { label: "도서", value: "book" },
-  { label: "필기구", value: "pencil" },
-  { label: "생활/가전", value: "life" },
-  { label: "의류", value: "clothes" },
-  { label: "전자기기", value: "digital" },
-  { label: "뷰티/미용", value: "beauty" },
-  { label: "부기굿즈", value: "goods" }
+  { label: "도서", value: "도서" },
+  { label: "필기구", value: "필기구" },
+  { label: "생활/가전", value: "생활가전" },
+  { label: "의류", value: "의류" },
+  { label: "전자기기", value: "전자기기" },
+  { label: "뷰티/미용", value: "뷰티미용" },
+  { label: "부기굿즈", value: "부기굿즈" }
 ];
